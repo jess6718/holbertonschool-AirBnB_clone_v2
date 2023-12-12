81b982e74ce5048a38e3e31b2398ee5e611b88f2
jess6718 <6718@holbertonstudents.com>
2023-12-12 11:12:49 +0100

    docs: pycode style

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,9 +9,11 @@ app = Flask(__name__)
 def hello_world():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_HBNB():
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(port=5000, host='0.0.0.0')
