5faa4c26d18b465be45216de35822f488bfdfb59
jess6718 <6718@holbertonstudents.com>
2023-12-12 12:33:49 +0100

    correct cities

--- models/state.py
+++ models/state.py
@@ -3,7 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
+from models.city import City
+from models import storage
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -11,13 +13,15 @@ class State(BaseModel, Base):
     State class
     """
     __tablename__ = "states"
-
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete')
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='all, delete')
+    else:
+        name = ""
 
     @property
     def cities(self):
-        from models.city import City
+        """Function for cities"""
         city_list = [city for city in storage.all(
             City).values() if city.state_id == self.id]
         return city_list
