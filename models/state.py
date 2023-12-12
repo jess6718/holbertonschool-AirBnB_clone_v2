7ce7c7c2971e25a97155ba7bedfb087580aecbb6
jess6718 <6718@holbertonstudents.com>
2023-12-12 13:11:49 +0100

    add more fix

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel, Base):
@@ -10,15 +11,14 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete')
 
-    @property
-    def cities(self):
-        """getter attribute to return list of City instances"""
-        from models import storage
-        from models.city import City
-        cities_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete')
+
+    else:
+        @property
+        def cities(self):
+            from models.city import City
+            city_list = [city for city in storage.all(
+                City).values() if city.state_id == self.id]
+            return city_list
